278be53cb8a76b75b3e06a70e8e94315834a5e2b
Mathews-Velez <velezmathew@gmail.com>
2023-01-15 18:55:16 -0500

    initial request test to api works

---
+++ __main__.py
@@ -0,0 +1,14 @@
+import requests
+
+#the url being used to make the api request
+URL = "https://www.dnd5eapi.co/api/monsters"
+
+#reciving a param for the api request
+CR = input("Please enter the the challenge rating you wish your monster to have: ")
+
+PARAMS = {'challenge_rating':CR}
+
+r = requests.get(url = URL, params = PARAMS)
+data = r.json()
+
+print("Got %s results back"%data['count'])
