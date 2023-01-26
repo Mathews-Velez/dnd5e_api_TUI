e97bcb86326d3a20ce24f372bd080e6894fdc0f4
mathew <velezmathew@gmail.com>
2023-01-26 12:40:30 -0500

    [Feat]Added auto comepletion, migrated tools to a seperate folder

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,5 @@
-from api_req_handler import api_req
-from json_data_formatter import format_data
+from utils.api_req_handler import api_req
+from utils.json_data_formatter import format_data
 
 def main():
     # display initial welcome message
@@ -11,18 +11,30 @@ def main():
     # print(data)
 
     #first menu for selecting the first layer of endpoints
-    endpoints = ["monsters", "spells"]
+    menus = ["monster_menu", "spell_menu"]
 
-    endpoint = endpoints[int(show_menu(endpoints))]
-
+    show_menu()
+    
     data = api_req(endpoint)
 
     format_data(data, endpoint)
-    
+
 def show_menu(options):
        for i in range(len(options)):
            print(f"{i} --- {options[i]}")
        return input("Endpoint: ")
 def monster_menu():
-   print()
+
+    #submenu for monster querying
+    #allow user to type the name of the monster they want to search for, offer auto complete
+    #display the live count of monster to be queried
+    #allow user to either see all the names of the monsters, while the list is being shown, we can prompt the user to continue down the list or imput the number related to the monster
+   options =["Search by name", "Filter list by challenge rating"]
+   print("Choose one of the following option to query the monsters endpoint:")
+   for option in options:
+       print(f"{option.index}--- {option}")
+   choice = input("Option: ")
+
+def search_by_name():#wait for the user to give their initail input and then activate the auto complete for them
+        index=input("Monster's name: ")
 main()
